0f22b21a9dcf2362a06c6737ad59bffcea661e25
nikolajnowicki <114876157+nikolajnowicki@users.noreply.github.com>
2024-01-04 09:41:11 +0100

    Added a few routes

---
+++ simply-calories/src/app/calorie-tracker/page.tsx
@@ -0,0 +1,7 @@
+export default function CalorieTracker() {
+  return (
+    <>
+      <h1>Calorie Tracker Page</h1>
+    </>
+  );
+}
---
+++ simply-calories/src/app/fasting-tracker/page.tsx
@@ -0,0 +1,7 @@
+export default function FastingTracker() {
+  return (
+    <>
+      <h1>Fasting Tracker Page</h1>
+    </>
+  );
+}
