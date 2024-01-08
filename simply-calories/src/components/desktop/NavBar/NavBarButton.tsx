5e26f07466f989b5e09ac35a7571cebb8ec6c29f
nikolajnowicki <114876157+nikolajnowicki@users.noreply.github.com>
2024-01-08 11:24:47 +0100

    Added light theme styling

--- simply-calories/src/components/desktop/NavBar/NavBarButton.tsx
+++ simply-calories/src/components/desktop/NavBar/NavBarButton.tsx
@@ -10,7 +10,7 @@ export const NavBarButton = ({ text, href }: NavBarButtonProps) => {
   return (
     <li className="list-none">
       <Link href={href} passHref>
-        <div className="flex justify-center items-center px-4 py-1 mx-1 h-16 text-md bg-transparent dark:hover:text-DarkTextCol2 cursor-pointer dark:focus:text-DarkTextCol2">
+        <div className=" relative flex justify-center items-center px-4 py-1 mx-1 h-16 text-md hover:text-cyan-700 text-LightTextCol bg-transparent dark:text-white dark:text-opacity-87 dark:hover:text-DarkTextCol2 cursor-pointer dark:focus:text-DarkTextCol2">
           {text}
         </div>
       </Link>
